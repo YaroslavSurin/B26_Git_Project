1135fe2c1fbd92cc2f981cbbd6228dc2208233b2
Yaroslav Surin <allstarcream@gmail.com>
2022-03-26 16:10:19 -0400

    First file updated. Some code added.

--- src/day0/First.java
+++ src/day0/First.java
@@ -2,8 +2,10 @@ package day0;
 
 public class First {
 
-    //TODO: Work on this tomorrow.
-    // add some cool code!
+    public static void main(String[] args) {
+        System.out.println("GIT IS FUN!");
+        System.out.println("FOR SURE!");
+    }
 
     //extra line 1
     //extra line 2
