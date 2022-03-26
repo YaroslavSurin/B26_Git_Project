b857216c57ffaf81c258f1a31ed061501157c898
Yaroslav Surin <allstarcream@gmail.com>
2022-03-26 16:32:52 -0400

    File been adjusted.

--- src/day0/JamesBond.java
+++ src/day0/JamesBond.java
@@ -1,12 +1,13 @@
 package day0;
-public class JamesBond{
-  public static void main (String [] args){
-  
-  System.out.println ("James Bond has hacked our project");
 
-  System.out.println ("We have new changes in the remote!");
-  
-  }
+public class JamesBond {
+    public static void main(String[] args) {
+
+        System.out.println("James Bond has hacked our project");
+
+        System.out.println("We have new changes in the remote!");
+
+    }
 
 }
 
